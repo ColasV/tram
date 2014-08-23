bf21f866a014dd84f19f05e3385201778297636d
Nicolas Vignes <nicolas.vignes@ensimag.grenoble-inp.fr>
2014-08-23 21:34:47 +0200

    Ajout de la BD et du premier model

---
+++ src/Tram/TramBundle/Entity/Ligne.php
@@ -0,0 +1,184 @@
+<?php
+
+namespace Tram\TramBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Ligne
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Tram\TramBundle\Entity\LigneRepository")
+ */
+class Ligne
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="code", type="string", length=255)
+     */
+    private $code;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="logo", type="string", length=255)
+     */
+    private $logo;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="departure", type="string", length=255)
+     */
+    private $departure;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="arrival", type="string", length=255)
+     */
+    private $arrival;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Ligne
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set code
+     *
+     * @param string $code
+     * @return Ligne
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    /**
+     * Get code
+     *
+     * @return string 
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * Set logo
+     *
+     * @param string $logo
+     * @return Ligne
+     */
+    public function setLogo($logo)
+    {
+        $this->logo = $logo;
+
+        return $this;
+    }
+
+    /**
+     * Get logo
+     *
+     * @return string 
+     */
+    public function getLogo()
+    {
+        return $this->logo;
+    }
+
+    /**
+     * Set departure
+     *
+     * @param string $departure
+     * @return Ligne
+     */
+    public function setDeparture($departure)
+    {
+        $this->departure = $departure;
+
+        return $this;
+    }
+
+    /**
+     * Get departure
+     *
+     * @return string 
+     */
+    public function getDeparture()
+    {
+        return $this->departure;
+    }
+
+    /**
+     * Set arrival
+     *
+     * @param string $arrival
+     * @return Ligne
+     */
+    public function setArrival($arrival)
+    {
+        $this->arrival = $arrival;
+
+        return $this;
+    }
+
+    /**
+     * Get arrival
+     *
+     * @return string 
+     */
+    public function getArrival()
+    {
+        return $this->arrival;
+    }
+}
