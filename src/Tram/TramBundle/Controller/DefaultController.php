11fe2b9f22805065ea660b983d4da1d2bee23266
Nicolas Vignes <nicolas.vignes@ensimag.grenoble-inp.fr>
2014-08-23 17:30:15 +0200

    Ajout du layout

--- src/Tram/TramBundle/Controller/DefaultController.php
+++ src/Tram/TramBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction($name)
     {
-        return $this->render('TramBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('TramBundle:Tram:index.html.twig', array('name' => $name));
     }
 }
